eaf5def87feefd608b832d292cc400b845915bb6
Aleksa Jovicic <aleksa.j@outlook.com>
2022-04-30 19:46:18 +0200

    Update theme switch story

--- src/stories/ThemeSwitch.stories.tsx
+++ src/stories/ThemeSwitch.stories.tsx
@@ -1,11 +1,17 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import ThemeSwitch from "~/components/ThemeSwitch";
+import AppThemeProvider from "~/providers/AppThemeProvider";
 
 export default {
   title: "components/ThemeSwitch",
   component: ThemeSwitch
 } as ComponentMeta<typeof ThemeSwitch>;
 
-const Template: ComponentStory<typeof ThemeSwitch> = args => <ThemeSwitch {...args} />;
+const Template: ComponentStory<typeof ThemeSwitch> = args => (
+  <AppThemeProvider>
+    {" "}
+    <ThemeSwitch {...args} />
+  </AppThemeProvider>
+);
 export const ThemeSwitchStory = Template.bind({});
 ThemeSwitchStory.storyName = "ThemeSwitch";
