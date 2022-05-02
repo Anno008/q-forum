853ec43dce58e8bdef572da5dd5efacdee603c51
Aleksa Jovicic <aleksa.j@outlook.com>
2022-05-02 18:25:12 +0200

    Trim unnecessary api call parts and improve test coverage for errors

--- src/api/__tests__/apiCall.test.ts
+++ src/api/__tests__/apiCall.test.ts
@@ -2,7 +2,7 @@ import { apiCall } from "~/api/apiCall";
 import { apiUrl } from "~/constants/config";
 
 describe("apiCall tests", () => {
-  it("Should return json object when calling getJSON", async () => {
+  it("Should return json object when calling apiCall", async () => {
     const jsonMock = {
       value: "test",
       id: 1
@@ -21,6 +21,30 @@ describe("apiCall tests", () => {
     });
 
     expect(response).toEqual(jsonMock);
+    expect(fetchSpy).toHaveBeenCalledWith(`${apiUrl}/${url}`, {
+      body: undefined,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "*/*"
+      },
+      method: "GET"
+    });
+  });
+
+  it("Should throw error json when response has a body", async () => {
+    const errorJsonMock = "Error";
+
+    const url = "test";
+    const fetchSpy = jest.spyOn(global, "fetch").mockImplementation(() =>
+      Promise.resolve({
+        ...fetch.prototype,
+        json: () => errorJsonMock,
+        status: 404,
+        body: true
+      })
+    );
+    await expect(apiCall({ url })).rejects.toBe(errorJsonMock);
+
     expect(fetchSpy).toHaveBeenCalledWith(`${apiUrl}/${url}`, {
       body: undefined,
       headers: {
--- src/api/apiCall.ts
+++ src/api/apiCall.ts
@@ -13,12 +13,12 @@ export const apiCall = <T>({ url, body, method }: FetchConfig): Promise<T> =>
     method: method || "GET"
   }).then(response => handleApiResponse<T>(response));
 
-const handleApiResponse = async <T>(response: Response): Promise<T> => {
+const handleApiResponse = <T>(response: Response): Promise<T> => {
   if (response.status >= 200 && response.status <= 204) {
-    return await response.json();
+    return response.json();
   }
   if (response.body) {
-    throw await response.json();
+    throw response.json();
   } else {
     throw response;
   }
